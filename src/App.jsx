332a3fe6553b8cac8e9eaaff02dc64cad0525828
Chris Pieri <chrispieri3@gmail.com>
2022-11-05 16:34:32 -0400

    Add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import faceeswap from './assets/faceeswap.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <img src={faceeswap} className="logo" alt="faceeswap logo" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
