4afd4152d92d42b36a40ce36c58654dbd4b21b5e
Chris Pieri <chrispieri3@gmail.com>
2022-11-08 11:03:10 -0500

    enhance the form

--- src/Components/MashupForm.jsx
+++ src/Components/MashupForm.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import AutocompleteInput from './UI/AutocompleteInput';
 
-export default function MashupForm({ options, onSubmit }) {
-  const [firstCelebrity, setFirstCelebrity] = useState('');
-  const [secondCelebrity, setSecondCelebrity] = useState('');
+export default function MashupForm({ options, onSubmit, verifyAnswers }) {
+  const [firstCelebrity, setFirstCelebrity] = useState(null);
+  const [secondCelebrity, setSecondCelebrity] = useState(null);
 
   const firstCelebrityHandler = (celebrity) => setFirstCelebrity(celebrity);
   const secondCelebrityHandler = (celebrity) => setSecondCelebrity(celebrity);
@@ -13,13 +13,18 @@ export default function MashupForm({ options, onSubmit }) {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    onSubmit([firstCelebrity, secondCelebrity]);
+    const answers = [firstCelebrity, secondCelebrity];
+    onSubmit(answers);
+    if (verifyAnswers(answers)) {
+      setFirstCelebrity(null);
+      setSecondCelebrity(null);
+    }
   };
 
   return (
     <form onSubmit={submitHandler} autoComplete="off">
-      <AutocompleteInput options={options} onChange={firstCelebrityHandler} />
-      <AutocompleteInput options={options} onChange={secondCelebrityHandler} />
+      <AutocompleteInput value={firstCelebrity} options={options} onChange={firstCelebrityHandler} />
+      <AutocompleteInput value={secondCelebrity} options={options} onChange={secondCelebrityHandler} />
       <button disabled={isDisabled} type="submit">
         Submit
       </button>
@@ -30,4 +35,5 @@ export default function MashupForm({ options, onSubmit }) {
 MashupForm.propTypes = {
   options: PropTypes.array.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  verifyAnswers: PropTypes.func.isRequired,
 };
--- src/Components/UI/AutocompleteInput.jsx
+++ src/Components/UI/AutocompleteInput.jsx
@@ -1,24 +1,38 @@
 import { Autocomplete, TextField } from '@mui/material';
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
-export default function AutocompleteInput({ options, onChange }) {
+export default function AutocompleteInput({ value, options, onChange }) {
+  const [placeholder, setPlaceholder] = useState('Who could it be?');
   const changeHandler = (event, value) => {
     onChange(value);
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPlaceholder(options[Math.floor(Math.random() * options.length)]);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [options]);
+
   return (
     <Autocomplete
       autoHighlight
+      value={value}
       options={options}
       onChange={changeHandler}
       renderInput={(params) => (
-        <TextField sx={{ input: { color: 'rgba(255, 255, 255, 0.87)' } }} {...params} placeholder="Michael Jackson" />
+        <TextField sx={{ input: { color: 'rgba(255, 255, 255, 0.87)' } }} {...params} placeholder={placeholder} />
       )}
     />
   );
 }
 
 AutocompleteInput.propTypes = {
+  value: PropTypes.string,
+  options: PropTypes.array.isRequired,
   onChange: PropTypes.func.isRequired,
-  options: PropTypes.array,
 };
