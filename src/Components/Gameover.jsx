a99e27a226f82b4dcf3b26d54752e07080c82357
Chris Pieri <chrispieri3@gmail.com>
2022-11-27 02:28:43 -0500

    Update final page

--- src/Components/Gameover.jsx
+++ src/Components/Gameover.jsx
@@ -1,15 +1,52 @@
 import FadeUp from './Animations/FadeUp';
+import complete from '../assets/complete.png';
+import styled from 'styled-components';
+import useSound from 'use-sound';
+import completeMP3 from '../assets/complete.mp3';
+import VolumeContext from '../Context/VolumeContext';
+import { useContext, useEffect, useState } from 'react';
 
 export default function Gameover() {
+  const volumeContext = useContext(VolumeContext);
+
+  const [hasSoundPlayed, setHasSoundPlayed] = useState(false);
+  const [completeSound] = useSound(completeMP3, { soundEnabled: volumeContext.isVolumeOn });
+
+  useEffect(() => {
+    let timer;
+    if (!hasSoundPlayed) {
+      timer = setTimeout(() => {
+        setHasSoundPlayed(true);
+        completeSound();
+      }, 2500);
+    }
+
+    return () => clearTimeout(timer);
+  }, [completeSound]);
+
   return (
-    <FadeUp delay={0.5}>
-      <h2>{"That's the ball game!"}</h2>
-      <FadeUp delay={2}>
+    <Container>
+      <FadeUp delay={2.5}>
+        <Img src={complete} alt="complete" />
+      </FadeUp>
+      <FadeUp delay={4.5}>
         <p>Thanks for playing! Come back soon for more face swaps.</p>
       </FadeUp>
-      <FadeUp delay={3.5}>
-        <p>If you had fun please share with your friends.</p>
+      <FadeUp delay={5.5}>
+        <p>Please share with your friends.</p>
       </FadeUp>
-    </FadeUp>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  @media (max-width: 480px) {
+    width: 350px;
+  }
+`;
