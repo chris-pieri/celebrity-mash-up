2c642ad6fd40cbfbac0b8c4a272f8924412f58ba
Chris Pieri <chrispieri3@gmail.com>
2022-11-27 00:11:02 -0500

    Refactore theme context (#32)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeContextProvider } from './Context/ThemeContext';
 import { VolumeContextProvider } from './Context/VolumeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <VolumeContextProvider>
-      <App />
+      <ThemeContextProvider>
+        <App />
+      </ThemeContextProvider>
     </VolumeContextProvider>
   </React.StrictMode>,
 );
